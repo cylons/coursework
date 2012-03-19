49416c769a879975d02d5ce087e1b2dd84967c0a
Anthony Shaper <ruby@cylons.me.uk>
2012-03-19 17:35:26 +0000

    Added first exercise

---
+++ ltp/ch01/numbers01.rb
@@ -0,0 +1,16 @@
+
+# Hours in a year
+puts 365.25 * 24
+
+# Minutes in a decade
+puts 10  * 365.25 * 24 * 60
+
+
+# My age in seconds  approx.
+puts 53 * 365.25 * 24 * 60 * 60
+
+# No of chocolates I expect to eat approx 
+puts 50 * 365.25 * 3
+
+# Persons age in years if they have lived for 1031 million seconds
+puts 1031000000.0 / (365.25 * 24 * 60 * 60)
