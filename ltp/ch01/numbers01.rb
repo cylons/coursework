17d027d2f7703367b9f9d5281c322fbc93802731
Anthony Shaper <ruby@cylons.me.uk>
2012-03-19 21:07:05 +0000

    Added Constants

--- ltp/ch01/numbers01.rb
+++ ltp/ch01/numbers01.rb
@@ -1,16 +1,28 @@
 
 # Hours in a year
-puts 365.25 * 24
+
+DaysInYear =  365.25
+HoursInDay = 24
+MinutesInHour = 60
+SecondsInMinute = 60
+MyAge = 53
+puts 'Hours in day '  + (DaysInYear * HoursInDay).to_s
 
 # Minutes in a decade
-puts 10  * 365.25 * 24 * 60
+
+
+puts 'Minutes in Decade ' + (10  * DaysInYear * HoursInDay * MinutesInHour).to_s
 
 
 # My age in seconds  approx.
-puts 53 * 365.25 * 24 * 60 * 60
+puts 'My Age in seconds ' + (MyAge * DaysInYear * HoursInDay * MinutesInHour * SecondsInMinute).to_s
 
-# No of chocolates I expect to eat approx 
-puts 50 * 365.25 * 3
+# No of chocolates I expect to eat approx
+
+AvChocksPerDay = 3
+YearsLeft = 45
+puts 'Chocolates to be eaten ' + (YearsLeft * DaysInYear * AvChocksPerDay).to_s
 
 # Persons age in years if they have lived for 1031 million seconds
-puts 1031000000.0 / (365.25 * 24 * 60 * 60)
+puts 'Age is ' + (1031000000.0 / (DaysInYear * HoursInDay * MinutesInHour * SecondsInMinute)).to_s
+
